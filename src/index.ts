028271d6cedbd52f30a7391e84a5b416e376442e
ajit <ajitmohapatra1024@gmail.com>
2024-09-24 18:08:23 +0530

    comit

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,55 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import { createServer } from 'http';
-import { KafkaClient, Producer } from 'kafka-node';
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { Request, Response } from "express"
+import { AppDataSource } from "./data-source"
+import { Routes } from "./routes"
+import { User } from "./entity/User"
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+AppDataSource.initialize().then(async () => {
 
-const kafkaClient = new KafkaClient({ kafkaHost: 'localhost:9092' });
-const producer = new Producer(kafkaClient);
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
 
-// import express from 'express';
-import { PrismaClient } from '@prisma/client';
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-// const app = express();
-const prisma = new PrismaClient();
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
 
-async function main() {
-  try {
-    await prisma.$connect();
-    console.log('Successfully connected to the database!');
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+    // setup express app here
+    // ...
 
-main();
-app.get('/', (req, res) => {
-  res.send('Hello World!!');
-});
+    // start express server
+    app.listen(3000)
 
-io.on('connection', (socket) => {
-  console.log('New socket connection:', socket.id);
-});
+    // insert new users for test
+    await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Timber",
+            lastName: "Saw",
+            age: 27
+        })
+    )
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+    await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Phantom",
+            lastName: "Assassin",
+            age: 24
+        })
+    )
+    app.use('/',(req,res)=>{
+      res.send('server listening')
+    })
 
-producer.on('ready', () => {
-  console.log('Kafka producer is ready');
-});
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+
+}).catch(error => console.log(error))
